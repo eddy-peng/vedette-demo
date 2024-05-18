754b0bee994294e0508e57b26e1cd7cd020e1071
Eddy Peng <eddyp2@uw.edu>
2024-05-17 18:23:14 -0700

    final data adds

--- frontend/src/screens/ViewReport.js
+++ frontend/src/screens/ViewReport.js
@@ -20,7 +20,6 @@ export default function ViewReport() {
             <div className="rawReportContainer">
                 <img className="topRightIllustration" src={TopRight} alt="shapes" />
                 <img className="leftIllustration" src={Left} alt="shapes" />
-                <img className="bottomRightIllustration" src={BottomRight} alt="shapes" />
                 <h1>Security: CRX3 File Signature Verification Bypass via Embedded ZIP64 Payload</h1>
                 <div className="metaDataContainer">
                     <div className="metaDataItem">
@@ -87,15 +86,76 @@ export default function ViewReport() {
                         <p>The installed extension is set up to use a local update server of "http://localhost:8000/my_sample_app.xml"</p>
                     </ul>
                     <p>3. Verify the extension has been successfully installed. When navigating to any website, such as https://www.google.com, you should see the following green banner:</p>
-                    <p>Sample extension has been installed! :)</p>
+                    <ul>
+                        <p>Sample extension has been installed! :)</p>
+                    </ul>
                     <p>4. Create the attack CRX file ("attack.crx") with an embedded ZIP64 payload by running the following:</p>
-                    <p>$ python3 payload_embedder.py my_sample_app.crx payload.zip attack.crx</p>
+                    <ul>
+                        <p>$ python3 payload_embedder.py my_sample_app.crx payload.zip attack.crx</p>
+                    </ul>
                     <p>5. Start up a Python http server to host the attack files:</p>
-                    <p>$ python3 -m http.server 8000</p>
+                    <ul>
+                        <p>$ python3 -m http.server 8000</p>
+                    </ul>
                     <p>6. Close and restart Chrome to trigger an immediate extension update. The extension will update to the malicious version.</p>
                     <p>7. Verify the malicious extension has been successfully installed. When navigating to any website, such as https://www.google.com, you will see the following red banner:</p>
-                    <p>Page contents have been pwned! :(</p>
+                    <ul>
+                        <p>Page contents have been pwned! :(</p>
+                    </ul>
+                    <p>POSSIBLE ATTACK SCENARIOS</p>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>Attacking a self-hosted enterprise extension (unmitigated)</p>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>A likely attack scenario for this vulnerability would be the case where an enterprise extension, self-hosted outside of the Chrome Web Store, has been made available to company employees with an update URL set to an HTTP server on the company's intranet. An employee leaves the building with their laptop and heads over to a coffee shop. A local attacker, also in this coffee shop, sets up their IP or host name to match the company's internal update server. The next time Chrome runs extension updates on the employee's device, it will download the maliciously modified extension from the attacker's device. Because it still contains a valid signature, the employee's laptop will upgrade to the malicious extension.</p>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>Attacking Chrome Web Store (CWS) hosted extensions (highly mitigated)</p>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>Mitigations:</p>
+                    <ol>
+                        <li>Would require HTTPS MITM.</li>
+                        <li>Chrome immediately detects an installed attacker-modified extension as being corrupt and disables it due to it containing an invalid "verified_contents.json" file, which is needed for CWS-installed extensions.</li>
+                    </ol>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>Attacking internal Chrome components (highly mitigated)</p>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>Mitigations:</p>
+                    <ol>
+                        <li>Would require HTTPS MITM.</li>
+                        <li>Chrome uses the Omaha CUP protocol (https://chromium.googlesource.com/chromium/src.git/+/master/docs/updater/protocol_3_1.md) which signs each HTTPS request and response body. This heavily mitigates any tampering from taking place.</li>
+                    </ol>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>Windows user to system LPE (near-miss)</p>
+                    <p>**-------------------------** ---------------------------------</p>
+                    <p>A low-privileged executable can use the "Google Chrome Elevation Service" to run a CRX file containing an executable as the highly-privileged "nt authority\system" user. The CRX file is first validated to ensure it has been signed by the correct hard-coded signature ("GetRecoveryCRXHash" from https://github.com/chromium/chromium/blob/main/chrome/elevation_service/elevated_recovery_impl.cc).</p>
+                    <p>This scenario could not be demonstrated \*only\* because the smallest publicly-available CRX file signed with this key was found to be 73.2kB (https://github.com/google/omaha/blob/main/omaha/testing/unittest_support/CodeRed.crx3), with a payload approximately 7kB too big to be exploitable.</p>
+                    <ul>
+                        <p>If the Chrome team can sign a slightly smaller CRX file with this same private key, you may be able to demonstrate the LPE with the following steps:</p>
+                    </ul>
+                    <ol>
+                        <li>Download the attached "windows_lpe_poc.exe", "windows_lpe_payload.zip", and "windows_lpe_poc.bat". Place these files in a directory "C:\poc\" along with the previously downloaded "payload_embedder.py" and "crx3_pb2.py".</li>
+                        <li>Inject the "windows_lpe_payload.zip" payload into the signed CRX file ("CodeRed.crx") as follows:</li>
+                        <ul>
+                            <p>$ python3 payload_embedder.py CodeRed.crx windows_lpe_payload.zip CodeRedAttack.crx</p>
+                        </ul>
+                        <li>Run "windows_lpe_test.exe" as follows to perform the COM call necessary for LPE (note that the full path of "CodeRedAttack.crx" must be used):</li>
+                        <ul>
+                            <p>$ .\windows_lpe_poc.exe C:\poc\CodeRedAttack.crx</p>
+                        </ul>
+                        <li>Verify that "windows_lpe_poc.bat" has run, placing the output of several commands including "whoami" into "C:\poc\output.txt". This file should indicate that the attacker's code was running as the "nt authority\system" user.</li>
+                    </ol>
+                    <p>SUGGESTED PATCH</p>
+                    <p>A simple yet effective way to patch this vulnerability would be to add an additional verification to the "VerifyCrx3" function, in order to detect any EOCD or EOCD64 token embedded within the header bytes. Since "VerifyCrx3" is called every time a CRX3 file has its signature validated, there is little chance of missing any affected code paths within the Chromium codebase. The header size in a CRX3 file is typically &lt; 2kB, so this would have very minimal performance impact.</p>
+                    <p>FIRST AFFECTED GIT COMMIT</p>
+                    <p>The CRX2 format (https://chromium.googlesource.com/chromium/src/+/8c83fe076ebe86c04e6d703be2bfb2f2e8729158/components/crx_file/crx_file.h) appears to only support a single key and signature in the file header, so it is unlikely an attacker would have enough control over this header to perform the necessary payload injection without invalidating the existing signature.</p>
+                    <p>Thus, it seems this vulnerability was introduced when the CRX3 format was first implemented, in the following commit: https://chromium.googlesource.com/chromium/src/+/11f044437561f28e16d0a2549714d860310fb3fd</p>
+                    <p>At this point, the verifier code was expanded to verify both CRX2 and CRX3 files, including CRX3 files with a malicious ZIP64 payload embedded in the header.</p>
+                    <p>CREDIT INFORMATION</p>
+                    <p>Reporter credit: Malcolm Stagg (@malcolmst) of SODIUM-24, LLC</p>
+                    <br/>
+                    <HorizontalLine />
+                    <p>End Of Raw Report</p>
                 </div>
+                <img className="bottomRightIllustration" src={BottomRight} alt="shapes" />
             </div>
         </div>
     );
